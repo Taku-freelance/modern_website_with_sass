393c8b0a33af1559989c137517ea7fd26f31863e
Taku Dev <72721832+Taku-freelance@users.noreply.github.com>
2021-06-27 23:19:21 +0900

    Index markdown sass

---
+++ LINKS.md
@@ -0,0 +1,26 @@
+## Links
+
+* Responsive Portfolio Website [1] - Intro & Sass Workflow Setup
+[YouTube](https://www.youtube.com/watch?v=gYzHS-n2gqU)
+
+* Responsive Portfolio Website [2] - Homepage & Main Sass
+[YouTube](https://www.youtube.com/watch?v=HguAyYnWBuU)
+
+* Responsive Portfolio Website [3] - Rotating Menu Button
+[YouTube](https://www.youtube.com/watch?v=7WaohfclZRs)
+
+* Responsive Portfolio Website [4] - Menu Overlay & Responsiveness
+[YouTube](https://www.youtube.com/watch?v=92J-3ajM0dI)
+
+* Responsive Portfolio Website [5] - About Page With CSS Grid
+[YouTube](https://www.youtube.com/watch?v=QOdnrMC9O7A)
+
+* Responsive Portfolio Website [6] - Work & Contact Pages
+[YouTube](https://www.youtube.com/watch?v=oxTG7TVr7PQ)
+
+* Responsive Portfolio Website [7] - Website Deployment
+[YouTube](https://www.youtube.com/watch?v=qGYNbrT9P6Y)
+
+
+## Gitgub
+[node-sass](https://github.com/sass/node-sass)
---
+++ dist/js/main.js
@@ -0,0 +1,45 @@
+/* ------------ Select DOM Item -------------*/
+// Grab the box
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+// Grab the item like an array 
+const navItem = document.querySelectorAll('.nav-item');
+
+/* ------------ Set initial State Of Menu -------------*/
+// open is true, close is false
+let showmenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //  Set Menu Status
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //  Set Menu Status
+        showMenu = true;
+        
+    }
+}
+
+
+
+/* ------------ Select DOM Item -------------*/
+/* ------------ Select DOM Item -------------*/
+
+/* ------------ Select DOM Item -------------*/
+/* ------------ Select DOM Item -------------*/
